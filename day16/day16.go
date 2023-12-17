99c1794f79951fb8354abe09172236a097078a96
Aaron Beitch <aaronb@arista.com>
2023-12-17 11:32:49 -0800

    Day 16, Part 2

--- day16/day16.go
+++ day16/day16.go
@@ -14,13 +14,55 @@ func main() {
 		panic(err)
 	}
 	g := aoc2023.Grid2D(bytes.Split(byts, []byte("\n")))
-	fmt.Println("Part 1:", energize(g))
+	p := aoc2023.Pos2D{X: 0, Y: 0}
+	best := energize(g, p, aoc2023.East)
+	fmt.Println("Part 1:", best)
+	for {
+		p = p.South()
+		if p.Y == len(g) {
+			p = p.North()
+			break
+		}
+		if c := energize(g, p, aoc2023.East); c > best {
+			best = c
+		}
+	}
+	for {
+		p = p.East()
+		if p.X == len(g[0]) {
+			p = p.West()
+			break
+		}
+		if c := energize(g, p, aoc2023.North); c > best {
+			best = c
+		}
+	}
+	for {
+		p = p.North()
+		if p.Y < 0 {
+			p = p.South()
+			break
+		}
+		if c := energize(g, p, aoc2023.West); c > best {
+			best = c
+		}
+	}
+	for {
+		p = p.West()
+		if p.X < 0 {
+			break
+		}
+		if c := energize(g, p, aoc2023.South); c > best {
+			best = c
+		}
+	}
+	fmt.Println("Part 2:", best)
 }
 
-func energize(g aoc2023.Grid2D) int {
+func energize(g aoc2023.Grid2D, p aoc2023.Pos2D, d aoc2023.Dir) int {
 	// lit is map of locations already visited and directions from the location.
 	lit := make(map[aoc2023.Pos2D][4]bool)
-	walk(g, lit, aoc2023.Pos2D{X: 0, Y: 0}, aoc2023.East)
+	walk(g, lit, p, d)
 	return len(lit)
 }
 
