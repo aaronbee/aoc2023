285addf1205ca8bc71c3c4774728b2fed6f3781f
Aaron Beitch <aaronb@arista.com>
2023-12-17 14:25:42 -0800

    Day 17, Part 2

--- day17/day17.go
+++ day17/day17.go
@@ -15,7 +15,8 @@ func main() {
 		panic(err)
 	}
 	g := aoc2023.Grid2D(bytes.Split(byts, []byte("\n")))
-	fmt.Println("Part 1:", walk(g))
+	fmt.Println("Part 1:", walk(g, next1))
+	fmt.Println("Part 2:", walk(g, next2))
 }
 
 type pos struct {
@@ -29,7 +30,7 @@ type step struct {
 	p    pos
 }
 
-func (s *step) next(g aoc2023.Grid2D, yield func(*step)) {
+func next1(s *step, g aoc2023.Grid2D, yield func(*step)) {
 	n := func(d aoc2023.Dir) {
 		n := step{
 			p: pos{
@@ -67,7 +68,56 @@ func (s *step) next(g aoc2023.Grid2D, yield func(*step)) {
 	}
 }
 
-func walk(g aoc2023.Grid2D) int {
+func next2(s *step, g aoc2023.Grid2D, yield func(*step)) {
+	n := func(d aoc2023.Dir) {
+		if s.p.d == d {
+			n := step{
+				p: pos{
+					p:     s.p.p.Step(d),
+					d:     d,
+					count: s.p.count + 1,
+				},
+			}
+			cost, ok := g.Get(n.p.p)
+			if !ok {
+				return
+			}
+			n.cost = s.cost + int(cost-'0')
+			yield(&n)
+			return
+		}
+		cost := s.cost
+		p := s.p.p
+		for i := 0; i < 4; i++ {
+			p = p.Step(d)
+			c, ok := g.Get(p)
+			if !ok {
+				return
+			}
+			cost += int(c - '0')
+		}
+		yield(&step{cost: cost, p: pos{p: p, d: d, count: 4}})
+	}
+	if s.p.count < 10 {
+		n(s.p.d)
+	}
+	switch s.p.d {
+	case aoc2023.North:
+		n(aoc2023.East)
+		n(aoc2023.West)
+	case aoc2023.East:
+		n(aoc2023.North)
+		n(aoc2023.South)
+	case aoc2023.South:
+		n(aoc2023.East)
+		n(aoc2023.West)
+	case aoc2023.West:
+		n(aoc2023.North)
+		n(aoc2023.South)
+	}
+}
+
+func walk(g aoc2023.Grid2D, next func(s *step, g aoc2023.Grid2D, yield func(*step))) int {
 	seen := map[pos]int{}
 	q := aoc2023.NewHeap(func(a, b *step) int {
 		aV := a.p.p.X + a.p.p.Y - a.cost
@@ -87,7 +137,7 @@ func walk(g aoc2023.Grid2D) int {
 			continue
 		}
 		seen[s.p] = s.cost
-		s.next(g, func(s *step) { q.Push(s) })
+		next(s, g, func(s *step) { q.Push(s) })
 	}
 	panic("path to end not found")
 }
